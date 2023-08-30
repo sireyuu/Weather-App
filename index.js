5b15f08fede23e60a9236abd89b44eadc8bbee2f
sireyuu <73392334+sireyuu@users.noreply.github.com>
2023-08-31 00:51:25 +0800

    json

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
-search.addEventListener('click', () =>{
+search.addEventListener('click', () => {
+
     const APIKey = '0bc652e4f09b9a28f6cd0b8551b922be';
     const city = document.querySelector('.search-box input').value;
 
-    if(city === '')
+    if (city === '')
         return;
 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`) .then(response.json()).then(json => {
-            if(json.cod === '404'){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+
+            if (json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
                 weatherDetails.style.display = 'none';
@@ -20,7 +24,7 @@ search.addEventListener('click', () =>{
                 error404.classList.add('fadeIn');
                 return;
             }
-            
+
             error404.style.display = 'none';
             error404.classList.remove('fadeIn');
 
@@ -28,7 +32,7 @@ search.addEventListener('click', () =>{
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
-            const wind = document.querySelector('weather-details .wind span');
+            const wind = document.querySelector('.weather-details .wind span');
 
             switch (json.weather[0].main) {
                 case 'Clear':
@@ -65,6 +69,9 @@ search.addEventListener('click', () =>{
             weatherBox.classList.add('fadeIn');
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
-        })
 
-    })
+
+        });
+
+
+});
